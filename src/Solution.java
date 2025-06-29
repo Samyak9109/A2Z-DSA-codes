880d4bb0440bd6428bfbd77c0a4552d2828c94fb
Samyak Jain <samyak.9109@gmail.com>
2025-06-29 20:03:40 +0530

    adding new basic question solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,12 @@
-public class Solution {
-    static void solve(int [] nums) {
-        int low = 0, mid = 0, high = nums.length - 1;
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        for (int i = 0; i <= 30; i++){
+            int ans = Math.pow(2, i);
 
-        // Loop until mid crosses high
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                // Swap arr[low] and arr[mid], move both forward
-                int temp = nums[low];
-                nums[low] = nums[mid];
-                nums[mid] = temp;
-                low++;
-                mid++;
-            } else if (nums[mid] == 1) {
-                // Just move mid pointer
-                mid++;
-            } else {
-                // Swap arr[mid] and arr[high], move high backward
-                int temp = nums[mid];
-                nums[mid] = nums[high];
-                nums[high] = temp;
-                high--;
-                // Do not increment mid here as the new element at mid needs checking
+            if (ans == n){
+                return true;
             }
         }
+        return false;
     }
 }
